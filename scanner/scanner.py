4f84523a950e3c1a3bc4d7aa45ea936b2074b313
Eugene Litvin <eugene.litvin@interactiv4.com>
2023-11-19 22:02:26 +0200

    Refactor

--- scanner/scanner.py
+++ scanner/scanner.py
@@ -13,13 +13,13 @@ class Scanner:
     ):
         self.__provider = Web3(Web3.HTTPProvider(http_provider))
 
-    def get_block_number(self):
+    def get_block_number(self) -> int:
         return self.__provider.eth.get_block_number()
 
     def load_block(
             self,
             block_number: int = None
-    ):
+    ) -> None:
         self.__data_provider.block = self.__provider.eth.get_block(
             'latest' if block_number is None else block_number
         )
@@ -28,7 +28,7 @@ class Scanner:
             self,
             pool: int = 1,
             offset: int = 0
-    ):
+    ) -> None:
         for transaction_index in range(offset, pool + offset):
             transaction_entity = self.__provider.eth.get_transaction(
                 self.__data_provider.block.transactions[transaction_index]
